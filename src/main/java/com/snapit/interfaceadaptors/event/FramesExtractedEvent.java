ee4425f9dd004aa2cc7716ccc4e1971075b372b9
GuiRodriguero <gui.rodriguero@hotmail.com>
2025-02-06 00:58:24 -0300

    Removing @Data from Event classes

--- src/main/java/com/snapit/interfaceadaptors/event/FramesExtractedEvent.java
+++ src/main/java/com/snapit/interfaceadaptors/event/FramesExtractedEvent.java
@@ -1,15 +1,13 @@
 package com.snapit.interfaceadaptors.event;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 
 import java.io.Serializable;
 
-@Data
 @AllArgsConstructor
 public class FramesExtractedEvent implements Serializable {
 
-    String id;
+    private String id;
 
     private String filename;
 
